f874c94520d6dedd27ac3391c95f78901bd78db8
Sumeet Maan <sumeetmaan13@gmail.com>
2022-04-23 19:35:52 -0500

    Added component

---
+++ src/components/counter.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    render() { 
+        return <h1>
+            Universal love
+        </h1>;
+    }
+}
+ 
+export default Counter;

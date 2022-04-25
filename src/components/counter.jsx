9d33ce0e55d917d9d782d4a9062d6874f930c08c
Sumeet Maan <sumeetmaan13@gmail.com>
2022-04-24 20:37:23 -0500

    Added button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,11 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
-    render() { 
-        return <h1>
-            Universal love
-        </h1>;
-    }
+  state = {
+    count: 0,
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <span>{this.formatCount()}</span> <button>Increment</button>
+      </React.Fragment>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
 }
- 
+
 export default Counter;

107ac295e1918a03e6d9e10080942bbcd8fe1fe3
Sumeet Maan <sumeetmaan13@gmail.com>
2022-04-25 18:32:07 -0500

    Multiple components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,12 +3,25 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
+    tags: ["tag1", "tag2", "tag3"],
   };
+
+  handleIncrement = (product) => {
+    console.log(product);
+    this.setState({ count: this.state.count + 1 });
+  };
+  styles = { fontSize: 50, fontWeight: "bold", fontColor: "black" };
   render() {
     return (
-      <React.Fragment>
-        <span>{this.formatCount()}</span> <button>Increment</button>
-      </React.Fragment>
+      <div>
+        <span style={this.styles}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement({ id: 1 })}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </div>
     );
   }
 
